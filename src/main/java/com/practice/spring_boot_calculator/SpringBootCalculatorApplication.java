9846f1781806af246f0de0cca8a0c23639c3ae69
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-12-08 01:32:26 +0900

    [Start] calculator using Spring Boot
    - Java21
    - Spring Boot
    - gradle
    
    to practice spring boot

---
+++ src/main/java/com/practice/spring_boot_calculator/SpringBootCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.practice.spring_boot_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCalculatorApplication.class, args);
+	}
+
+}
